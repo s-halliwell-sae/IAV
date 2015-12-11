9446a931f5973fce9e420c3f4b27a6c444a23e76
Shaun Allen <mad_max2455@hotmail.com>
2015-12-11 14:53:42 +1000

    Added advanced toggle to playlist and updated playlist package

---
+++ Playlist/Assets/Scripts/PlaylistAdvanced.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlaylistAdvanced : MonoBehaviour {
+
+	public GameObject panel;
+	public AudioSource audioSource;
+	public Toggle toggle;
+	public Button setDefault;
+
+	public Slider volume;
+	public Slider panning;
+
+	void Awake()
+	{
+		audioSource = GameObject.Find("Audio Source").GetComponent<AudioSource> ();
+		volume.value = audioSource.volume;
+		panning.value = audioSource.panStereo;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(toggle.isOn)
+		{
+			panel.SetActive(true);
+		}
+		else
+		{
+			panel.SetActive(false);
+		}
+
+		if(audioSource.volume != volume.value)
+		{
+			UpdateVolume (volume.value);
+		}
+
+		if(audioSource.panStereo != panning.value)
+		{
+			UpdatePanning (panning.value);
+		}
+	}
+
+	void UpdateVolume(float value)
+	{
+		audioSource.volume = value;
+	}
+
+	void UpdatePanning(float value)
+	{
+		audioSource.panStereo = value;
+	}
+
+	public void SetDefault()
+	{
+		float vol = 0.5f;
+		float pan = 0f;
+
+		UpdateVolume (0.5f);
+		volume.value = vol;
+		UpdatePanning (0f);
+		panning.value = pan;
+	}
+}
