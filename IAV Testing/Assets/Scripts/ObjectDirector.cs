a89ad360fa7b4d26c47918a37c5e263668ca7523
abwhit <1002406@student.sae.edu.au>
2015-12-15 21:36:49 +1000

    Updated Scene and ObjectDirector Script
    - Updated scene to be more vibrant, added lots of objects
    - Changed all arrays to lists so objects can be dynamically allocated to list
    - Added a populate list method that uses tags
    - No extra white space
    - Updated method names to be more generalised

--- IAV Testing/Assets/Scripts/ObjectDirector.cs
+++ IAV Testing/Assets/Scripts/ObjectDirector.cs
@@ -12,52 +12,43 @@ public class ObjectDirector : MonoBehaviour {
     public float movementMultiplier = 10;
     // Adds more dynamic colour changes
     public float colourMultiplier = 10;
-    // Array of gameobjects
-    public GameObject[] objectsInScene;
+    // Lists to store the objects that will be changing
+    public List<GameObject> scaleObjects = new List<GameObject>();
+    public List<GameObject> moveObjects = new List<GameObject>();
+    public List<GameObject> colourObjects = new List<GameObject>();
+    public List<GameObject> lights = new List<GameObject>();
     // Dictionary of gameobject starting positions
     private Dictionary<GameObject, Vector3> objectsStartingPosition = new Dictionary<GameObject, Vector3>();
-
-
+    // Dictionary to store the lights
     private Dictionary<GameObject, Light> LightGameObjects = new Dictionary<GameObject, Light>();
-
     // Cube to test methods
     public GameObject testCube;
+    public GameObject testLight;
     // Will be used for beat detections
     float sum;
     float fMax = 200;
-
     // For testing purposes to it is easy to change methods during play
     public bool moveMethods = false;
     public bool scaleMethods = true;
     public bool colourMethods = false;
     public bool singleObject = true;
-
     public bool songPlaying = false;
-
-    
     // Enum so colour cna be changed in scene
     public enum PColour {red, green, blue }
     public PColour primaryColour;
-
-
-    public GameObject[] lights;
-
-    public GameObject testLight;
-
-
-
     // Use this for initialization
     void Start () {
-
-        // Fill the dictionary
-        PopulateStartingPosDictionary(objectsStartingPosition, objectsInScene);
-
-        PopulateLightDictionary(LightGameObjects, lights);
+        // Populate the scale and movement lists
+        PopulateGOListWithTag(scaleObjects, "Scale");
+        PopulateGOListWithTag(moveObjects, "Move");
+        // Fill the dictionary to store initial positions of objects
+        PopulateStartingPosDictionary(objectsStartingPosition, moveObjects);
+        //PopulateLightDictionary(LightGameObjects, lights);
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        // Check if there is a song playing
         if(fftInt.samples.Length > 0)
         {
             songPlaying = true;
@@ -67,25 +58,23 @@ public class ObjectDirector : MonoBehaviour {
         if (songPlaying)
         {
             //LightColour(testLight, 1);
-
-            LightArrayColourChange(lights, 0, PColour.blue);
-
+            //LightArrayColourChange(lights, 0, PColour.blue);
+            // Methods used to change the colour of objects
             if (colourMethods)
             {
                 if (singleObject)
                 {
-                    ChangeColour(testCube, 1, primaryColour);
+                    ChangeColour(testCube, 0, primaryColour);
                 }
                 else
                 {
-
-                    ObjectArrayColourChange(objectsInScene, 20, primaryColour);
+                    MultipleObjectColourChange(scaleObjects, 0, primaryColour);
+                    MultipleObjectColourChange(moveObjects, 0, primaryColour);
                 }
-
             }
             if (scaleMethods)
             {
-
+                // Methods used to scale objects
                 if (singleObject)
                 {
                     // Single object scale change
@@ -94,11 +83,10 @@ public class ObjectDirector : MonoBehaviour {
                 else
                 {
                     // Array of objects scale change
-                    ObjectArraySizeChange(objectsInScene, 0);
+                    MultipleObjectScaleChange(scaleObjects, 0);
                 }
             }
         }
-
         // Beat detection testing
         //BandVol(100, 1000);
         //Debug.Log(sum);
@@ -119,8 +107,8 @@ public class ObjectDirector : MonoBehaviour {
             }
             else
             {
-                // Array of objects movement
-                ObjectArrayMovement(objectsInScene, 0);
+                    // Array of objects movement
+                    MultipleObjectMovement(moveObjects, 0);
             }
         }
     }
@@ -134,7 +122,7 @@ public class ObjectDirector : MonoBehaviour {
             // Set a starting scale, so the object doesnt grow out of proportion
             Vector3 startingScale = new Vector3(1, 1, 1);
             // Set a new vector to store the values from the bins
-            Vector3 scaleChange = new Vector3(fftInt.avgBins[binNum] * scaleMultiplier, fftInt.avgBins[binNum] * scaleMultiplier * 10, fftInt.avgBins[binNum] * scaleMultiplier);
+            Vector3 scaleChange = new Vector3(fftInt.avgBins[binNum] * scaleMultiplier, fftInt.avgBins[binNum] * scaleMultiplier, fftInt.avgBins[binNum] * scaleMultiplier);
             // Set the localScale of the object in the scene using the startingScale and scaleChange
             objectInScene.transform.localScale = new Vector3(startingScale.x + scaleChange.x, startingScale.y + scaleChange.y, startingScale.z + scaleChange.z);
         }
@@ -146,12 +134,12 @@ public class ObjectDirector : MonoBehaviour {
     }
     
     // Function to change scale of all objects in an array
-    void ObjectArraySizeChange(GameObject[] objects, int lowestBinNum)
+    void MultipleObjectScaleChange(List<GameObject> objects, int lowestBinNum)
     {
-        if (lowestBinNum + objects.Length <= fftInt.avgBins.Length)
+        if (lowestBinNum + objects.Count <= fftInt.avgBins.Length)
         {
             // Iterate through list and move objects
-            for (int i = 0; i < objects.Length; i++)
+            for (int i = 0; i < objects.Count; i++)
             {
                 ChangeSize(objects[i], i);
             }
@@ -159,8 +147,7 @@ public class ObjectDirector : MonoBehaviour {
         // Error message so people understand if they are doing something wrong
         else
         {
-
-            Debug.LogError("Not enough bins to support the highest bin number, The highest number the LowestBinNum can be is:" + (fftInt.avgBins.Length - objects.Length));
+            Debug.LogError("Not enough bins to support the highest bin number, The highest number the LowestBinNum can be is:" + (fftInt.avgBins.Length - objects.Count));
         }
     }
 
@@ -183,14 +170,13 @@ public class ObjectDirector : MonoBehaviour {
         }
     }
 
-
     // Function for moving multiple objects using bins
-    void ObjectArrayMovement(GameObject[] objects, int lowestBinNum)
+    void MultipleObjectMovement(List<GameObject> objects, int lowestBinNum)
     {
-        if (lowestBinNum + objects.Length <= fftInt.avgBins.Length)
+        if (lowestBinNum + objects.Count <= fftInt.avgBins.Length)
         { 
         // Iterate through list and move objects
-        for(int i = 0; i < objects.Length; i++)
+        for(int i = 0; i < objects.Count; i++)
         {
             MoveObject(objects[i], i);
         }
@@ -198,19 +184,24 @@ public class ObjectDirector : MonoBehaviour {
         // Error message so people understand if they are doing something wrong
         else
         {
-
-            Debug.LogError("Not enough bins to support the highest bin number, The highest number the LowestBinNum can be is:" + (fftInt.avgBins.Length - objects.Length));
+            Debug.LogError("Not enough bins to support the highest bin number, The highest number the LowestBinNum can be is:" + (fftInt.avgBins.Length - objects.Count));
         }
     }
 
-
     // Method for filling the dictionary using an array of objects
-    void PopulateStartingPosDictionary(Dictionary<GameObject, Vector3> dictionary, GameObject[] arrayOfObjects)
-    {       
-        // Run a loop and add the objects and their starting positions into the dictionary
-        for (int i = 0; i < arrayOfObjects.Length; i++)
+    void PopulateStartingPosDictionary(Dictionary<GameObject, Vector3> dictionary, List<GameObject> listOfObjects)
+    {
+        if (listOfObjects.Count > 0)
         {
-            dictionary.Add(arrayOfObjects[i], arrayOfObjects[i].transform.position);
+            // Run a loop and add the objects and their starting positions into the dictionary
+            for (int i = 0; i < listOfObjects.Count; i++)
+            {
+                dictionary.Add(listOfObjects[i], listOfObjects[i].transform.position);
+            }
+        }
+        else
+        {
+            Debug.LogError("ErrorWithStartingDictionary");
         }
     }
 
@@ -223,8 +214,7 @@ public class ObjectDirector : MonoBehaviour {
         Color newColour = new Color(fftInt.avgBins[binNum], fftInt.avgBins[binNum], fftInt.avgBins[binNum], fftInt.avgBins[binNum]);
         // Switch to check what colour the user wants to use
         switch(mainColour)
-        {
-                
+        {     
             case PColour.red:
                 newColour.r = newColour.r * colourMultiplier;
                 break;
@@ -244,76 +234,82 @@ public class ObjectDirector : MonoBehaviour {
     }
 
     // Function for changing the colour of a whole array at once
-    void ObjectArrayColourChange(GameObject[] objects, int lowestBinNum, PColour mainColour)
-    {
-
-        if (lowestBinNum + objects.Length <= fftInt.avgBins.Length)
+    void MultipleObjectColourChange(List<GameObject> objects, int lowestBinNum, PColour mainColour)
+    {      
+        // Set oarticular bins to colours so there are multiple colours in the scene at once
+        if (lowestBinNum + objects.Count <= fftInt.avgBins.Length)
         {
-            // loop through the array and run the ChangeColour method
-            for (int i = 0; i < objects.Length; i++)
+            // loop through the array and run the ChasngeColour method
+            for (int i = 0; i < objects.Count; i++)
             {
-
-                ChangeColour(objects[i], i + lowestBinNum, mainColour);
-            }
+                if (i < 15)
+                {
+                    ChangeColour(objects[i], i + lowestBinNum, PColour.blue);
+                }
+                else if (i < 30)
+                {
+                    ChangeColour(objects[i], i + lowestBinNum, PColour.red);
+                }
+                else if (i > 30)
+                {
+                    ChangeColour(objects[i], i + lowestBinNum, PColour.green);
+                }
+                
+            }   
         }
         else
-        {
-
-            Debug.LogError("Not enough bins to support the highest bin number, The highest number the LowestBinNum can be is:" + (fftInt.avgBins.Length - objects.Length));
+        {  
+            Debug.LogError("Not enough bins to support the highest bin number, The highest number the LowestBinNum can be is:" + (fftInt.avgBins.Length - objects.Count));
         }
     }
 
-
-    void PopulateLightDictionary(Dictionary<GameObject, Light> dictionary, GameObject[] arrayOfObjects)
+    // Populating the light dictionary
+    void PopulateLightDictionary(Dictionary<GameObject, Light> dictionary, List<GameObject> listOfObjects)
     {
-
-        for(int i = 0; i < arrayOfObjects.Length; i++)
+        // Loop through a list of objects, get the object, and the light attached to the object
+        for (int i = 0; i < listOfObjects.Count; i++)
         {
 
-            dictionary.Add(arrayOfObjects[i], arrayOfObjects[i].GetComponent<Light>());
-
-
+            dictionary.Add(listOfObjects[i], listOfObjects[i].GetComponent<Light>());
         }
-
-
-
     }
 
-
+    // Method to change the colour of a light
     void LightColour(GameObject light, int binNum, PColour colour)
     {
-        Light objectLight = light.GetComponent<Light>();
-
-        Color newColour = new Color();
-
-         switch(colour)
-        {
-                
-            case PColour.red:
-                newColour = Color.red;
-                break;
-
-            case PColour.green:
-                newColour = Color.green;
-                break;
-            case PColour.blue:
-                newColour = Color.blue;
-                break;
-            default:
-                break;
+        // Check there is a light
+        if (light != null)
+        {   // Set a light vairables to the components
+            Light objectLight = light.GetComponent<Light>();
+            // Set a new colour
+            Color newColour = new Color();
+            // Use switch to change the colour using the Pcolour enum
+            switch (colour)
+            {
+                case PColour.red:
+                    newColour = Color.red;
+                    break;
+                case PColour.green:
+                    newColour = Color.green;
+                    break;
+                case PColour.blue:
+                    newColour = Color.blue;
+                    break;
+                default:
+                    break;
+            }
+            // Change the colour
+            objectLight.color = newColour;
         }
-
-        objectLight.color = newColour;
-
-
     }
 
-    void LightArrayColourChange(GameObject[] lights, int lowestBinNum, PColour colour)
+    // Changing lights in an array
+    void MultipleLightObjectChange(List<GameObject> lights, int lowestBinNum, PColour colour)
     {
-        if (lowestBinNum + lights.Length <= fftInt.avgBins.Length)
+        if (lowestBinNum + lights.Count <= fftInt.avgBins.Length)
         {
             // loop through the array and run the ChangeColour method
-            for (int i = 0; i < lights.Length; i++)
+            for (int i = 0; i < lights.Count; i++)
             {
                 
                 LightColour(lights[i], i + lowestBinNum, colour ); 
@@ -321,12 +317,38 @@ public class ObjectDirector : MonoBehaviour {
         }
         else
         {
-
-            Debug.LogError("Not enough bins to support the highest bin number, The highest number the LowestBinNum can be is:" + (fftInt.avgBins.Length - lights.Length));
+            Debug.LogError("Not enough bins to support the highest bin number, The highest number the LowestBinNum can be is:" + (fftInt.avgBins.Length - lights.Count));
         }
 
     }
 
+    // Method for populating gameObject lists using tags
+    void PopulateGOListWithTag(List<GameObject> list, string tag)
+    {
+        // Check the tag, and change the loop accordingly
+        // Needs to be made more efficient
+        if (tag == "Scale")
+        {
+            foreach (GameObject item in GameObject.FindGameObjectsWithTag("Scale"))
+            {
+                list.Add(item);
+            }
+        }
+        else if (tag == "Move")
+        {
+            foreach (GameObject item in GameObject.FindGameObjectsWithTag("Move"))
+            {
+                list.Add(item);
+            }
+        }
+        else if (tag == "Colour")
+        {
+            foreach (GameObject item in GameObject.FindGameObjectsWithTag("Colour"))
+            {
+                list.Add(item);
+            }
+        }
+    }
 
     //Trying to get beat detection
 
